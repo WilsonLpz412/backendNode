abe8a0e44479bbb749a121473ee3bd77ed84375f
WilsonLpz412 <wold412@gmail.com>
2021-03-12 08:23:52 -0500

    Recibir información desde el cliente: body y query

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/message', function (req, res) {
   res.send('Lista de mensajes');
 });
 router.post('/message', function (req, res) {
-  res.send('Mensaje añadido');
+  console.log(req.query);
+  console.log(req.body);
+  res.send('Mensaje ' + req.body.text + ' añadido');
 });
 //app.use('/', function (req, res) {
 //  res.send('Hola');
