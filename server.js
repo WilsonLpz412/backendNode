1f3eb31adb3bdc1303efd309c9fc7e62e6542b87
WilsonLpz412 <wold412@gmail.com>
2021-03-12 19:59:18 -0500

    Respuesta coherentes y centralizadas

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const response = require('./network/response');
+
 const router = express.Router();
 
 var app = express();
@@ -9,13 +11,19 @@ app.use(router);
 
 router.get('/message', function (req, res) {
   console.log(req.headers);
-  res.header({'custom-header': 'Nuestro valor personalizado' })
-  res.send('Lista de mensajes');
+  res.header({'custom-header': 'Nuestro valor personalizado' });
+  //res.send('Lista de mensajes');
+  response.success(req, res, 'Lista de mensajes');
 });
 router.post('/message', function (req, res) {
   console.log(req.query);
-  console.log(req.body);
-  res.status(201).send([{error: '', body: 'Creado correctamente'}]);
+  //console.log(req.body);
+  if (req.query.error == "ok") {
+    response.error(req, res, 'Error simulado');
+  } else {
+    //res.status(201).send([{error: '', body: 'Creado correctamente'}]);
+    response.success(req, res, 'Creado correctamente', 201);
+  }
 });
 //app.use('/', function (req, res) {
 //  res.send('Hola');
