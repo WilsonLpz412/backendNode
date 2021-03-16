20d5b74b3feb5dbea5c0c97ffca8fe56d178184f
WilsonLpz412 <wold412@gmail.com>
2021-03-15 19:30:59 -0500

    Controladores: Definiendo la lógica de negocio

--- components/message/network.js
+++ components/message/network.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const response = require('../../network/response');
+const controller = require('./controller');
 const router = express.Router();
 
 router.get('/', function (req, res) {
@@ -9,14 +10,15 @@ router.get('/', function (req, res) {
   response.success(req, res, 'Lista de mensajes');
 });
 router.post('/', function (req, res) {
-  console.log(req.query);
+  //console.log(req.query);
   //console.log(req.body);
-  if (req.query.error == "ok") {
-    response.error(req, res, 'Error inesperado', 500, 'Error simulado');
-  } else {
-    //res.status(201).send([{error: '', body: 'Creado correctamente'}]);
-    response.success(req, res, 'Creado correctamente', 201);
-  }
+  controller.addMessage(req.body.user, req.body.message)
+    .then((fullMessage) => {
+      response.success(req, res, fullMessage, 201);
+    })
+    .catch((e) => {
+      response.error(req, res, e, 400, 'Error en el controlador');
+    });
 });
 
 module.exports = router;
