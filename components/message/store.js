b289b598c0d869be1b9c4082b121b7e23c519c93
WilsonLpz412 <wold412@gmail.com>
2021-03-17 21:43:42 -0500

    MongoDB: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,25 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+db.Promise = global.Promise;
+db.connect('mongodb+srv://db_user_backendNode:OdTGDijnJnFUMSkh@cluster0.63u4m.mongodb.net/db_backendNode?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log('[db] Conectada con éxito');
+//.then(() => console.log('[db] Conectada con éxito'))
+//.catch(e => console.error('[db] ', e));
 
 function addMessage(message) {
-  list.push(message);
+  //list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessage() {
-  return list;
+async function getMessage() {
+  //return list;
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
